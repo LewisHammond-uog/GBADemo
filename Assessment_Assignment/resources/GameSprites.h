7724b07102c5b6071c570cf4f828506a35913ee9
Lewis Hammond <s1800707@connect.glos.ac.uk>
2019-12-19 13:42:20 +0000

    Fix Particles being Black
    Increased Pallet Length to stop particles being black

--- Assessment_Assignment/resources/GameSprites.h
+++ Assessment_Assignment/resources/GameSprites.h
@@ -4,11 +4,11 @@
 //======================================================================
 //
 //	GameSprites, 80x64@8, 
-//	+ palette 16 entries, not compressed
+//	+ palette 256 entries, not compressed
 //	+ 80 tiles Metatiled by 2x2 not compressed
-//	Total size: 32 + 5120 = 5152
+//	Total size: 512 + 5120 = 5632
 //
-//	Time-stamp: 2019-12-19, 01:23:02
+//	Time-stamp: 2019-12-19, 13:15:53
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -20,8 +20,8 @@
 #define GameSpritesTilesLen 5120
 extern const unsigned short GameSpritesTiles[2560];
 
-#define GameSpritesPalLen 32
-extern const unsigned short GameSpritesPal[16];
+#define GameSpritesPalLen 512
+extern const unsigned short GameSpritesPal[256];
 
 #endif // GRIT_GAMESPRITES_H
 
